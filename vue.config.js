762d6a1dc512b8499003a6b06c6d558369b7b15d
Manuel Mejia <manuel@buildinglink.com>
2020-03-23 20:58:13 -0400

    prerenderSPAPlugin

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,29 @@
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
+
 module.exports = {
   // publicPath: process.env.NODE_ENV === 'production' ? '/personal-website/' : '/',
+  configureWebpack: () => {
+    if (process.env.NODE_ENV !== 'production') return
+
+    return {
+      plugins: [
+        new PrerenderSPAPlugin({
+          staticDir: path.join(__dirname, 'docs'),
+          routes: ['/'],
+          minify: {
+            collapseBooleanAttributes: true,
+            collapseWhitespace: true,
+            decodeEntities: true,
+            keepClosingSlash: true,
+            sortAttributes: true
+          },
+          renderer: new Renderer({ headless: true })
+        })
+      ]
+    }
+  },
   chainWebpack: config => {
     const svgRule = config.module.rule('svg')
 
