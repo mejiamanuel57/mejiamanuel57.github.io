ca650192116c6931fd0a8dbde289b2d7f805e56f
Manuel Mejia <manuel@buildinglink.com>
2020-03-25 22:24:37 -0400

    adding Google Analytics + vue-gtag

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import './assets/css/tailwind.css'
 import vueSmoothScroll from 'vue2-smooth-scroll'
+import VueGtag from 'vue-gtag'
 
+Vue.use(VueGtag, {
+  config: { id: 'UA-119694085-1' }
+})
 Vue.use(vueSmoothScroll)
 Vue.config.productionTip = false
 
